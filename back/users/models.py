7e68553aaa6770f74fd9352f13c13d608de781dd
VitorMiguel34 <miguelvictor035@gmail.com>
2026-02-16 17:15:53 -0300

    fix: corrigindo modelo + serializer de usuario

--- back/users/models.py
+++ back/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
--- back/users/serializers.py
+++ back/users/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Task,User
+from .models import Task
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("id", "username", "email", "password")
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class TaskSerializer(ModelSerializer):
     class Meta:
